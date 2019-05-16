d067997d4744e55e18a76af503ea17ca9386145c
z1014794151 <821258496@qq.com>
2019-05-16 14:56:03 +0800

    java learn

---
+++ 1.java
@@ -0,0 +1,33 @@
+// 1.第一个java程序
+public class HelloWorld{
+    public static void main(String[] args){
+        System.out.println("Helloworld!");
+    }
+}
+
+//2.常量
+final double PI = 3.14;
+
+//3.字符串
+String s0 = "abc";
+String s1 = new String("abd");
+int len=s0.length();
+s0.equals(); == equalsIgnoreCase();
+String s1 = "World" + "!";
+String s2 = s0.concat(s1);
+char c = s.charAt(1);
+String s="abcdef";
+s.indexOf('a');
+s.lastIndexOf('a');
+s.substring((3,6));
+
+//4.输入
+Scanner in = new Scanner(System.in);
+String a = in.nextLine();
+
+//5.函数
+public void functionName (Object obj){
+    //方法体
+}
+
+//asdasdasdsadasdasdasdsad 加了行
